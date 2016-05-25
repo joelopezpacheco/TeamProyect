50b043598ae5e073fa3cd70392bc6912d3f0e09b
Joel Lopez <ing.sistemas.jlopez@gmail.com>
2016-05-25 14:21:13 -0500

    Prueba

--- app/src/main/java/cibertec/teamproyect/MainActivity.java
+++ app/src/main/java/cibertec/teamproyect/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //prueba en android  no suposiempre sno
+        //prueba para comit PAA TODO
+
     }
 }
