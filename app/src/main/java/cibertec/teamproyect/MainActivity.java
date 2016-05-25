0b822bdbae342cbecb8ce43e92099f07e25eb172
Joel Lopez <ing.sistemas.jlopez@gmail.com>
2016-05-25 10:27:03 -0500

    Prueba

--- app/src/main/java/cibertec/teamproyect/MainActivity.java
+++ app/src/main/java/cibertec/teamproyect/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //prueba de android java
+        //prueba de android java para todos
     }
 }
